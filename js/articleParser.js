8c0851ce61a261583fe60db35c16e94fdb109b65
fearyfiremast <fearyfiremast@gmail.com>
2025-08-13 20:51:46 -0700

    began working on post update mechanisms

--- js/articleParser.js
+++ js/articleParser.js
@@ -1,8 +1,34 @@
 const PATH = "JSON/articles.json"
 
+// Worth considering an id that serves this purpose
+const postFeed = document.querySelector("#postContent");
+
+// Represents JSON file loaded into mem
+let articleFile = null;
+
 /* Retrieves the JSON files stored in 
- *
+ * TODO: Implement
  */
 function loadArticlesJSON() {
+    return [];
+}
+
+
+function updatePosts() {
+    // checks if JSON file already exists
+    
+    if (articleFile === null) {
+        // May be messy as fetch API returns a promise
+        articleFile = loadArticlesJSON();
+    }
+
+    // handles errors with retrieval
+    if (articleFile.length === 0) {
+        postFeed.textContent += "HELLO :3";
+    }
+    
+    
 
 }
+
+updatePosts()
