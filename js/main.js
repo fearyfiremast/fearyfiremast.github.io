14c9dea405d299b6d3c0b182d06fb1fb1621d6ec
fearyfiremast <fearyfiremast@gmail.com>
2025-09-19 08:50:10 -0700

    updated banner code to be dynamic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-import * as Feed from "./postFeed.js";
+//import * as Feed from "./postManager.js";
 
-// Driver method
+/**
+ * General method that should be called on all pages. Loads HTML data such as the header
+ * as well as the nav and footer. Universal content
+ */
 async function initialize() {
 
-    /* For some reason running the method here calls the entire file in postFeed.
-    EG functions run that are called in the postFeed file.
+    applyBanner()
+    return;
+}
 
-    Additionally postFeed.js does not require the document be passed into it
-     */
-    Feed.updatePosts();
+/**  Allows the dynamic updating of banner
+ * 
+ */
+function applyBanner() {
+    // Useful when I want to make banners dynamic
+    //const pageTitle = document.title;
+    const bannerElement = document.getElementById("banner");
+    // Update in future to name banner based on pageTitle
+    // If banner not found imgPath reverts to default
+    let imgName = "default";
+
+    let toAppend = document.createElement("img");
+    toAppend.src = `\\images\\core\\banner\\${imgName}.jpg`;
+    bannerElement.appendChild(toAppend);
+    return;
 }
 
 initialize();
