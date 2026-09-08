ad29a02e6bb3a413fe918c3c98b653522601ab11
fearyfiremast <fearyfiremast@gmail.com>
2026-09-08 15:24:55 -0700

    Feat: Added descriptions for other projects

--- src/constants/Project.ts
+++ src/constants/Project.ts
@@ -1,19 +1,34 @@
-export type ProjectCardInfo = {
+export type ProjectInfo = {
   slug: string
   title: string;
   description: string;
   imgUrl: string;
   imgAlt?: string;
+  className?: string;
 }
 
-export const PROJECTS: ProjectCardInfo[] = [
+export const PROJECTS: ProjectInfo[] = [
   {
     slug: 'portfolio-site',
     title: 'portfolio site project',
     description: `The development history behind the creation of this website. 
                   Created in my spare time over the summer of 2025 using raw JavaScript, 
                   CSS, and HTML to create something that is both dynamic and responsive`,
+    imgUrl: '/projects/personal-site/article-feed-sketch.jpg',
+    imgAlt: ''
+  },
+  {
+    slug: 'blueprint-website-v2',
+    title: 'Blueprint website redesign',
+    description: 'As a junior developer I helped work with others to redo the club website',
     imgUrl: '',
     imgAlt: ''
-  }
+  },
+  {
+    slug: 'cip-cta',
+    title: 'Full stack development at CIP',
+    description: `Creation of a database, server, and content creation tool for internal use at CIP Modular`,
+    imgUrl: '',
+    imgAlt: ''
+  },
 ]
