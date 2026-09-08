85f6619e31e79b6d15571205acf1d04b51aae353
fearyfiremast <fearyfiremast@gmail.com>
2026-09-08 15:26:30 -0700

    Feat: Set single highlighted project and organized project layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,9 @@ import ATFHome from "../components/home/ATFHome"
 const Home = () => {
 
   const highlightedArticles: string[] = [
-    'portfolio-site'
+    'portfolio-site',
+    'blueprint-website-v2',
+    'cip-cta'
   ]
 
   return (
@@ -15,13 +17,13 @@ const Home = () => {
       {/* Cover */}
       <ATFHome />
       <hr className="my-4"/>
-      <HighlightedProject/>
+      <HighlightedProject slug="portfolio-site"/>
       {/* Project Showcase */}
-      <section>
-        <h2 className="font-semibold text-[30px] text-center mb-4">
-          Showcase
+      <section className="mt-16">
+        <h2 className="font-semibold text-[30px] text-center mb-9">
+          Projects
         </h2>
-        <div className="flex flex-col">
+        <div className="flex flex-row gap-4 h-100 mb-2">
           {PROJECTS.filter((item) => highlightedArticles.includes(item.slug)).map((item) => 
             <ProjectCard 
               key={item.slug}
@@ -30,9 +32,13 @@ const Home = () => {
               imgUrl={item.imgUrl}
               imgAlt={item.imgAlt}
               slug={item.slug}
+              className="flex-1 hover:flex-2 transition-all duration-200"
             />
           )}
         </div>
+        <button className="w-full text-[18px] bg-gray-500 rounded-xs text-white hover:cursor-pointer transition-colors duration-200 hover:bg-blue-primary">
+          View All
+        </button>
       </section>
     </PageTemplate>
   )
