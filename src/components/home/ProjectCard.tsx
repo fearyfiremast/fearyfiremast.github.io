ceb1881192c737bea4e7bdb7d965538f441e980c
fearyfiremast <fearyfiremast@gmail.com>
2026-09-08 15:25:44 -0700

    Feat: Standardized image behaviour and added default fallback image

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -1,10 +1,10 @@
-import type { ProjectCardInfo } from "../../constants/Project"
+import type { ProjectInfo } from "../../constants/Project"
 
-const ProjectCard = ({title, description, imgUrl, imgAlt}: ProjectCardInfo) => {
+const ProjectCard = ({title, description, imgUrl, imgAlt, className}: ProjectInfo) => {
   return (
-    <div className="flex gap-3 bg-white hover:cursor-pointer hover:ring-2 ring-gray-600 rounded-[5px] overflow-hidden p-4">
-      <div className="w-200 bg-gray-500">
-        <img src={imgUrl} alt={imgAlt} />
+    <div className={`flex gap-3 bg-white hover:cursor-pointer hover:ring-2 ring-gray-600 rounded-[5px] overflow-hidden p-4 ${className}`}>
+      <div className="min-w-40 max-w-40 object-cover bg-gray-500">
+        <img src={imgUrl ?? 'https://placehold.co/400'} alt={imgAlt} />
       </div>
       <div className="flex flex-col gap-4">
         <h1 className="text-xl underline font-semibold">{title}</h1>
