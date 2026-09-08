cab743decca3ce522ce66ea3631523cc7805f498
fearyfiremast <fearyfiremast@gmail.com>
2026-09-08 15:33:02 -0700

    Feat: By default has a vertical design

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -2,8 +2,8 @@ import type { ProjectInfo } from "../../constants/Project"
 
 const ProjectCard = ({title, description, imgUrl, imgAlt, className}: ProjectInfo) => {
   return (
-    <div className={`flex gap-3 bg-white hover:cursor-pointer hover:ring-2 ring-gray-600 rounded-[5px] overflow-hidden p-4 ${className}`}>
-      <div className="min-w-40 max-w-40 object-cover bg-gray-500">
+    <div className={`flex flex-col gap-3 bg-white hover:cursor-pointer hover:ring-2 ring-gray-600 rounded-[5px] overflow-hidden p-4 ${className}`}>
+      <div className="w-full overflow-clip h-[50%] object-cover bg-gray-500">
         <img src={imgUrl ?? 'https://placehold.co/400'} alt={imgAlt} />
       </div>
       <div className="flex flex-col gap-4">
